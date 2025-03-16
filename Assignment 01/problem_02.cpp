f73ff3a4516ba497cd01e174553afcd7057c0e74
Tahsin Maria <166499732+tahsin-maria@users.noreply.github.com>
2025-03-16 15:56:58 +0600

    Add files via upload

---
+++ Assignment 01/problem_02.cpp
@@ -0,0 +1,10 @@
+//Take length and breadth as input (both integers) and calculate the area of the rectangle
+#include<iostream>
+using namespace std;
+int main()
+{
+    float l,b;
+    cin>>l>>b;
+    cout<<"area: "<<l*b<<endl;
+}
+
